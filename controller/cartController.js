052b44d642449e342c01fa6f52fc085918c5ea70
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2022-12-24 22:15:39 +0530

    fix(response): updated api response

--- controller/cartController.js
+++ controller/cartController.js
@@ -9,29 +9,19 @@ const addItemsToCart = async (req, res, next) => {
 
     let errorMessage = '';
     if (validItem && validUser) {
-      const [cartObj] = await CartModel.find(
-        { userId: userId },
-        { _id: 0, __v: 0 }
-      );
-
-      const myCart = Object.assign(
-        {},
-        {
-          userId: userId,
-          items: [productId],
-        }
-      );
+      const cartObj = await CartModel.findOne({ userId: userId });
 
       if (cartObj) {
         if (cartObj.items.includes(productId)) {
           res.status(400).json({
+            success: false,
             message: 'Item already in cart',
+            items: cartObj.items,
           });
         } else {
           const updateCart = await CartModel.findOneAndUpdate(
             { userId: userId },
             {
-              userId: userId,
               $push: {
                 items: productId,
               },
@@ -41,21 +31,26 @@ const addItemsToCart = async (req, res, next) => {
               runValidators: true,
             }
           );
-          // res.status(201).json({
-          //   message: 'Item added to cart successfully!',
-          // });
 
-          res.status(200);
-          res.send(updateCart.items);
+          res.status(200).json({
+            success: true,
+            message: 'Item added to cart successfully!',
+            items: updateCart.items,
+          });
         }
       } else {
-        const newCart = await CartModel.create(myCart);
-        // res.status(201).json({
-        //   message: 'Item added to cart successfully!',
-        // });
+        const myCart = {
+          userId: userId,
+          items: [productId],
+        };
 
-        res.status(200);
-        res.send(newCart.items);
+        const newCart = await CartModel.create(myCart);
+
+        res.status(200).json({
+          message: 'Item added to cart successfully!',
+          success: true,
+          items: newCart.items,
+        });
       }
     } else {
       if (!validItem) {
@@ -65,16 +60,17 @@ const addItemsToCart = async (req, res, next) => {
       }
 
       res.status(400).json({
+        success: false,
         message: errorMessage,
       });
     }
   } catch (err) {
     res.status(400).json({
-      status: 'fail',
+      success: false,
       message: err.message,
     });
   }
-  next();
+  // next();
 };
 
 const removeItemsFromCart = async (req, res, next) => {
@@ -86,28 +82,24 @@ const removeItemsFromCart = async (req, res, next) => {
 
     let errorMessage = '';
     if (validItem && validUser) {
-      const [myCart] = await CartModel.find(
-        { userId: userId },
-        { _id: 0, __v: 0 }
-      );
-
       const updatedCart = await CartModel.findOneAndUpdate(
-        { userId: userId },
+        { userId },
         {
-          userId: userId,
-          items: myCart.items.filter(item => item !== productId),
+          $pull: {
+            items: productId,
+          },
         },
         {
           new: true,
           runValidators: true,
         }
       );
-      res.status(200);
-      res.send(updatedCart.items);
 
-      // res.status(201).json({
-      //   message: 'Item removed from cart successfully!',
-      // });
+      res.status(200).json({
+        success: true,
+        message: 'Item removed from cart successfully!',
+        items: updatedCart,
+      });
     } else {
       if (!validItem) {
         errorMessage = 'Invalid Item';
@@ -117,39 +109,41 @@ const removeItemsFromCart = async (req, res, next) => {
 
       res.status(400).json({
         message: errorMessage,
+        success: false,
       });
     }
   } catch (err) {
     res.status(400).json({
-      status: 'fail',
+      success: false,
       message: err.message,
     });
   }
-  next();
+  // next();
 };
 
 const getCartItemsByUserId = async (req, res, next) => {
   const { userId } = req.params;
 
   try {
-    const [cartItem] = await CartModel.find(
-      { userId: userId },
-      { _id: 0, __v: 0 }
-    );
+    const cart = await CartModel.findOne({ userId: userId });
 
-    if (validateUserId(userId) && cartItem) {
+    if (validateUserId(userId) && cart && cart.items) {
       res.status(200);
-      res.send(cartItem.items);
+      res.send({ success: true, items: cart.items });
     } else if (!validateUserId(userId)) {
-      res.status(400).json({ message: 'Invalid User' });
-    } else
-      res.status(400).json({ message: 'Could not find any items in cart' });
+      res.status(400).json({ success: false, message: 'Invalid User!' });
+    } else {
+      res
+        .status(400)
+        .json({ success: false, message: 'Could not find any items in cart' });
+    }
   } catch (err) {
     res.status(400).json({
-      message: err,
+      success: false,
+      message: err.message,
     });
   }
-  next();
+  // next();
 };
 
 module.exports = {
--- controller/productController.js
+++ controller/productController.js
@@ -4,64 +4,69 @@ const { generateProductId } = require('../utilities/helper');
 
 const getAllProducts = async (req, res, next) => {
   try {
-    const products = await ProductsModel.find({}, { _id: 0, __v: 0 });
+    const products = await ProductsModel.find({});
+
     if (products.length > 0) {
-      res.status(200);
-      res.send(products);
-    } else res.status(400).json({ message: 'No products available.' });
+      res.status(200).json({ success: true, items: products });
+    } else
+      res
+        .status(400)
+        .json({ success: false, message: 'No products available.' });
   } catch (err) {
-    res.status(400).json({
-      message: err,
-    });
+    res.status(400).json({ success: false, message: err });
   }
-  next();
+  // next();
 };
 
 const getProductDetails = async (req, res, next) => {
   const { productId } = req.params;
   try {
-    const products = await ProductsModel.find(
-      { productId: productId },
-      { _id: 0, __v: 0 }
-    );
+    const product = await ProductsModel.findById(productId);
 
-    const [myProduct] = products;
-    if (products.length > 0) {
-      res.status(200);
-      res.send(myProduct);
+    if (product) {
+      res.status(200).json({
+        status: true,
+        product,
+      });
     } else {
-      res.status(400).json({ message: 'No product' });
+      res.status(400).json({
+        status: false,
+        message: 'No product',
+      });
     }
   } catch (err) {
     res.status(400).json({
+      status: false,
       message: err,
     });
   }
-  next();
+  // next();
 };
 
 const addProduct = async (req, res, next) => {
   try {
     if (validateProducDetails(req.body)) {
-      const id = await generateProductId();
-      const item = Object.assign({}, { productId: id }, req.body);
+      const item = Object.assign({}, req.body);
       let newItem = await ProductsModel.create(item);
 
       res.status(201).json({
-        message: 'Product added Successfully! with ID: ' + id,
+        success: true,
+        message: 'Product added Successfully!',
+        productId: newItem._id,
       });
     } else {
       res.status(400).json({
+        success: false,
         message: 'Invalid Product details.',
       });
     }
   } catch (err) {
     res.status(400).json({
-      status: 'fail',
+      success: false,
       message: err,
     });
   }
-  next();
+  // next();
 };
 
 module.exports = {
