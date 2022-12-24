df60ce11f7fb9dded79c56a83a59676c82eda838
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2022-12-24 22:21:20 +0530

    fix(routes): updated routes with jwt

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const myReqLogger = require('./utilities/logger');
-const errorLogger = require('./utilities/errorLogger');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const router = express.Router();
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const productRouter = require('./routes/product');
-const cartRouter = require('./routes/cart');
-const wishlistRouter = require('./routes/wishlist');
+const indexRouter = require('./routes');
+const cookieParser = require('cookie-parser');
+const myReqLogger = require('./utilities/logger');
+const errorLogger = require('./utilities/errorLogger');
 
 dotenv.config();
 
@@ -25,26 +21,15 @@ mongoose
   .then(() => console.log('Connected to DB '))
   .catch(err => console.log('Cannot connect to DB ', err));
 
-// mongoose.connect('mongodb://localhost:27017/NeukartTestDB');
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use(cors());
 app.use(myReqLogger);
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/products', productRouter);
-app.use('/cart', cartRouter);
-app.use('/wishlist', wishlistRouter);
-
-router.all('*', (err, req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: 'Invalid path',
-  });
-  next(err);
-});
 
 app.use(errorLogger);
 
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const { authVerify } = require('../middlewares/authMiddleware');
+const userRoutes = require('./users');
+const cartRoutes = require('./cart');
+const productRoutes = require('./product');
+const wishlistRoutes = require('./wishlist');
+
 const indexRouter = express.Router();
 
-// Router level middleware
-// indexRouter.use((req, res, next) => {
-//   console.log(`index router - req method is ${req.method}`);
-//   next();
-// });
+indexRouter.use('/users', userRoutes);
+indexRouter.use('/products', productRoutes);
+indexRouter.use(authVerify);
+indexRouter.use('/cart', cartRoutes);
+indexRouter.use('/wishlist', wishlistRoutes);
+indexRouter.all('*', (req, res, next) => {});
 
 /* GET home page. */
 indexRouter.get('/', (req, res, next) => {
   res.status(200);
-  res.send({ title: 'NeuKart application' });
-  next();
+  res.send({ success: true, title: 'NeuKart application' });
+  // next();
 });
 
 module.exports = indexRouter;
