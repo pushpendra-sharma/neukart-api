69f33cb66bc92a2766ec8fc3e8fed5af1d139c70
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2022-12-24 22:12:32 +0530

    fix: updated document models

--- models/wishList.js
+++ models/wishList.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const wishlistSchema = new mongoose.Schema(
   {
-    userId: { type: String },
-    items: [],
+    userId: { type: Schema.Types.ObjectId, ref: 'user' },
+    items: [{ type: Schema.Types.ObjectId, ref: 'product' }],
   },
   {
     timestamps: true,
