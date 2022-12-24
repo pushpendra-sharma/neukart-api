d0bd95c51a320817c7a0f95f2bf5282601019d74
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2022-12-24 22:13:50 +0530

    feat(jwt): added jwt authorization

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,7 @@
 const UsersModel = require('../models/user');
+const CartModel = require('../models/cart');
+const WishListModel = require('../models/wishList');
+const { createAuthToken } = require('../middlewares/authMiddleware');
 
 const {
   validateExistingUserByEmail,
@@ -7,11 +10,8 @@ const {
   validateEmail,
   validateUserName,
 } = require('../utilities/validator');
-const { generateUserId } = require('../utilities/helper');
-const CartModel = require('../models/cart');
-const WishListModel = require('../models/wishList');
 
-const registerUser = async (req, res, next) => {
+const signup = async (req, res, next) => {
   try {
     const { password, mobileNumber, email, name } = req.body;
 
@@ -24,21 +24,27 @@ const registerUser = async (req, res, next) => {
       validateEmail(email) &&
       !isExistingUser
     ) {
-      const id = await generateUserId();
-      const myUser = Object.assign({}, { userId: id }, req.body);
-      let newUser = await UsersModel.create(myUser);
+      const myUser = Object.assign({}, req.body);
+      const newUser = await UsersModel.create(myUser);
 
       await CartModel.create({
-        userId: id,
+        userId: newUser._id,
         items: [],
       });
 
       await WishListModel.create({
-        userId: id,
+        userId: newUser._id,
         items: [],
       });
-      
-      res.status(201).json(newUser);
+
+      const token = 'Bearer ' + createAuthToken(newUser._doc);
+
+      res.set('Authorization', token).status(201).json({
+        success: true,
+        message: 'Signup successfull.',
+        user: newUser,
+        token,
+      });
     } else {
       if (!validateUserPassword(password)) {
         erroMessage =
@@ -53,44 +59,54 @@ const registerUser = async (req, res, next) => {
         erroMessage = 'User exists with this email id';
       }
 
-      res.status(400).json({
-        message: erroMessage,
-      });
+      res.status(400).json({ success: false, message: erroMessage });
     }
   } catch (err) {
     res.status(400).json({
-      status: 'fail',
+      success: false,
       message: err.message,
     });
   }
-  next();
+  // next();
 };
 
-const loginUser = async (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
-    const user = await validateExistingUserByEmail(email);
-    const userByEmailAndPassword = await UsersModel.find(
-      { email: email, password: password },
-      { _id: 0, __v: 0 }
-    );
+    const myUser = await UsersModel.findOne({ email: email });
 
-    const [myUser] = userByEmailAndPassword;
-    if (user && userByEmailAndPassword.length > 0) {
-      res.status(200);
-      res.send(myUser);
+    if (!myUser) {
+      throw new Error('Enter valid email.');
+    }
+
+    const passwordIsCorrect = await myUser.comparePassword(password);
+    if (!passwordIsCorrect) {
+      throw new Error('Incorrect password.');
+    }
+    if (myUser && passwordIsCorrect) {
+      const token = 'Bearer ' + createAuthToken(myUser._doc);
+
+      res.set('Authorization', token).status(200).json({
+        success: true,
+        message: 'User logged in successfully.',
+        user: myUser,
+        token,
+      });
     } else {
       res.status(400).json({
         message: 'Incorrect user email or password',
       });
     }
   } catch (err) {
-    res.status(400).json({
-      message: err,
-    });
+    res.status(400).json({ success: false, message: err.message });
   }
-  next();
+  // next();
 };
 
-module.exports = { registerUser, loginUser };
+const logout = (req, res) => {
+  res.cookie('jwt', '', { maxAge: 1 });
+  res.redirect('/');
+};
+
+module.exports = { signup, login, logout };
