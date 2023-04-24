048d8acd93ac6037c6531e41979fbb9bcb7439ae
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-04-25 00:48:37 +0530

    feat: updated express app

---
+++ index.ts
@@ -0,0 +1,23 @@
+import express, { Express, Response, Request, Application } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { connectToDB, logger } from './utils';
+import { router } from './routes';
+
+dotenv.config();
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors());
+
+app.use('/', router);
+
+const port = process.env.PORT || 4000;
+app.listen(port, async () => {
+  logger.info(`App running on port ${port}...`);
+  await connectToDB();
+});
---
+++ utils/index.ts
@@ -0,0 +1,4 @@
+export * from './connectToDB';
+export * from './logger';
+export * from './jwt';
+export * from './validator';
